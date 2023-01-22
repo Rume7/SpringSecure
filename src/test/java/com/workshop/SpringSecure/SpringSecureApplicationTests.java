b1e89757b702a8eb319dfff7551765c82995baac
Rume7 <electengrhume@gmail.com>
2023-01-22 18:55:52 +0100

    Adding startup files.

---
+++ src/test/java/com/workshop/SpringSecure/SpringSecureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.SpringSecure;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
